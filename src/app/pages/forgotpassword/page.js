b0824d91347bfce4929959e70d84c411304feb64
karan-pandey15 <pandeykaran1515@gmail.com>
2024-06-04 16:38:25 +0530

    update

--- src/app/pages/forgotpassword/page.js
+++ src/app/pages/forgotpassword/page.js
@@ -52,8 +52,7 @@ const ForgotPassword = () => {
   };
 
   return (
-    <>
-    <TopNavbar />
+    <> 
       <section className="bg-gray-50">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
